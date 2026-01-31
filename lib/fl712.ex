f7e2fdb08253dff6a330351a51b46fdbced75cd7
hayatroid <hayatroid@gmail.com>
2026-01-31 20:31:42 +0000

    `mix new . --sup`

---
+++ lib/fl712.ex
@@ -0,0 +1,18 @@
+defmodule Fl712 do
+  @moduledoc """
+  Documentation for `Fl712`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fl712.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fl712_test.exs
@@ -0,0 +1,8 @@
+defmodule Fl712Test do
+  use ExUnit.Case
+  doctest Fl712
+
+  test "greets the world" do
+    assert Fl712.hello() == :world
+  end
+end
